d55910cade70c64c4048a1a72a159e48b00159f6
Thorben Jändling <thorbenj@users.noreply.github.com>
2023-03-14 14:15:27 +0100

    set node context data too

--- contextConfig.js
+++ contextConfig.js
@@ -10,15 +10,26 @@ module.exports = function(RED) {
         var node = this;
         const valid_key = /^[_A-Za-z][^.]+$/;
 
-        if (!(n.scope === 'flow' || n.scope === 'global')) {
-            node.error("contextConfig: Invalid scope: "+n.scope)
-            retrun
-        }
         if (!(n.mode === 'startup' || n.mode === 'msg')) {
-            node.error("contextConfig: Invalid mode: "+n.mode)
-            retrun
+            node.error(`Invalid mode: ${n.mode}`)
+            return
         }
 
+        function _setMutliCtx(ctx, data){
+            if (! data instanceof Object) {
+                node.error("Context data is not a hash (object)")
+                return false
+            }
+            for (var k in data) {
+                if (! valid_key.test(k)) {
+                    node.warn(`Invalid key: ${k}`)
+                    continue
+                }
+
+                ctx.set(k, data[k])
+            }
+            return true
+        }
 
         function configureContext(msg){
 
@@ -33,17 +44,28 @@ module.exports = function(RED) {
                 return
             }
 
-            const ctx = node.context()
-            for (var k in data) {
-                if (! valid_key.test(k)) {
-                    node.warn("contextConfig: Invalid key: "+k)
-                    continue
-                }
-
-                ctx[n.scope].set(k, data[k])
+            switch (n.scope) {
+                case "node":
+                    for (var nid in data){
+                        const tn = RED.nodes.getNode(nid)
+                        if (! tn) {
+                            node.error(`Node id ${nid} not found`)
+                            continue
+                        }
+                        _setMutliCtx(tn.context(), data[nid])
+                    }
+                    break;
+                case "flow":
+                case "globl":
+                    _setMutliCtx((node.context())[n.scope], data)
+                    break;
+                default:
+                    node.error(`Invlid scope: ${n.scope}`)
+                    return
             }
 
         }
+
         this.on('input', function(msg) {
 
             if (n.mode !== 'msg')
